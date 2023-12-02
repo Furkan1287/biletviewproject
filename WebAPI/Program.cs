8244ec7582936d0fe9a56bbe9c4512008db2ff50
Murat Cinek <cinekmurat34@gmail.com>
2023-12-02 11:36:10 +0300

    service register extension added.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,25 +1,12 @@
-using Application.Services;
-using Domain.Entities;
+using Application.Extensions;
 using Infrastructure;
 using Microsoft.EntityFrameworkCore;
-using Shared.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddScoped<IEventService, EventService>();
-builder.Services.AddScoped<IGenericRepositoryAsync<Event>, GenericRepositoryBaseAsync<Event, ApplicationDbContext>>();
-
-builder.Services.AddScoped<ISeatedEventService, SeatedEventService>();
-builder.Services.AddScoped<IGenericRepositoryAsync<SeatedEvent>, GenericRepositoryBaseAsync<SeatedEvent, ApplicationDbContext>>();
-
-builder.Services.AddScoped<IStandingEventService, StandingEventService>();
-builder.Services.AddScoped<IGenericRepositoryAsync<StandingEvent>, GenericRepositoryBaseAsync<StandingEvent, ApplicationDbContext>>();
-
-builder.Services.AddScoped<IEventImageService, EventImageService>();
-builder.Services.AddScoped<IGenericRepositoryAsync<EventImage>, GenericRepositoryBaseAsync<EventImage, ApplicationDbContext>>();
-
+builder.Services.RegisterServices();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddCors(options =>
